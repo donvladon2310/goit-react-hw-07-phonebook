1cd5a77fb0bcbd21dd64f000558429e5da3d1117
donvladon2310 <vibliy.vlad@gmail.com>
2023-03-12 20:34:06 +0200

    іівпі

--- src/redux/contacts/contacts-operatins.js
+++ src/redux/contacts/contacts-operatins.js
@@ -1,4 +1,5 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
+
 import * as api from "../../shared/services/contacts";
 
 export const fetchAllContacts = createAsyncThunk(
@@ -6,27 +7,23 @@ export const fetchAllContacts = createAsyncThunk(
     async (_, thunkApi) => {
         try {
             const data = await api.getAllContacts();
-            console.log("fetchAllContacts data: ", data);
             return data;
         }
         catch ({ response }) {
-            console.log("fetchAllContacts error: ", response.data);
-            return thunkApi.rejectWithValue(response.data);
+            return thunkApi.rejectWithValue(response.data)
         }
     }
-);
+)
 
 export const fetchAddContact = createAsyncThunk(
     "contacts/add",
     async (data, { rejectWithValue }) => {
         try {
             const result = await api.addContact(data);
-            console.log("fetchAddContact result: ", result);
             return result;
         }
         catch ({ response }) {
-            console.log("fetchAddContact error: ", response.data);
-            return rejectWithValue(response.data);
+            return rejectWithValue(response.data)
         }
     },
     {
@@ -34,28 +31,26 @@ export const fetchAddContact = createAsyncThunk(
             const { contacts } = getState();
             const normName = name.toLowerCase();
             const result = contacts.items.find(({ name }) => {
-                return (name.toLowerCase() === normName);
-            });
+                return (name.toLowerCase() === normName)
+            })
             if (result) {
-                console.log(`${name} is already in contacts`);
-                alert(`${name} is already in contacts`);
+                alert(`${name} is alredy in contacts`);
                 return false;
             }
         }
     }
-);
+)
 
 export const fetchDeleteContact = createAsyncThunk(
     "contacts/delete",
     async (id, { rejectWithValue }) => {
         try {
             await api.deleteContact(id);
-            console.log("fetchDeleteContact id: ", id);
             return id;
         }
         catch ({ response }) {
-            console.log("fetchDeleteContact error: ", response.data);
             return rejectWithValue(response.data);
         }
     }
-);
+)
+
